906b6f85f1ebe36779df33801e3ba9980acdc430
glenio-juste <glenio.juste@gmail.com>
2021-07-13 21:25:29 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/juste/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juste.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
