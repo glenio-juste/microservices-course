ee82b19c68b0d02044c6417db499458e15d33eaf
glenio-juste <glenio.juste@gmail.com>
2021-09-15 22:52:35 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/juste/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/juste/hroauth/services/UserService.java
@@ -2,27 +2,45 @@ package com.juste.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.juste.hroauth.entities.User;
 import com.juste.hroauth.feignclients.UserFeignClients;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
-	private UserFeignClients userFeignClients;
+	private UserFeignClients userFeignClient;
+	
 	
 	public User findByEmail(String email) {
-		User user = userFeignClients.findByEmail(email).getBody();
+		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
 		return user;
+	}
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
+		
 	}
 
 }
